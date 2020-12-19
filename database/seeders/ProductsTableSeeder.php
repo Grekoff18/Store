5d35894d00058457c556751c1e02002c420da469
Nickolay Grekalo <kolagrekalo@gmail.com>
2020-12-19 22:35:44 +0200

    generate some data with seeding

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            DB::table("products")->insert([
+                "title" => "Product . {$i}",
+                "price" => rand(100, 900),
+                "in_stock" => 1,
+                "description" => Str::random(20)
+
+            ]);
+        }
+    }
+}
