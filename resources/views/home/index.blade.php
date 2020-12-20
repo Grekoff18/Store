adc7d501ea0004979408da6b3d5d9fe4f6ab06a2
Nickolay Grekalo <kolagrekalo@gmail.com>
2020-12-20 22:35:33 +0200

    created face product cards

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -132,9 +132,16 @@
                     <div class="product_grid">
 
                         @foreach($products as $product)
-                            <!-- Product -->
+                            @php
+                                $image = "";
+                                if (count($product->images) > 0) {
+                                    $image = $product->images[0]["img"];
+                                } else {
+                                    $image = "no_image.png";
+                                }
+                            @endphp
                             <div class="product">
-                                <div class="product_image"><img src="images/product_1.jpg" alt=""></div>
+                                <div class="product_image"><img src="/images/{{$image}}" alt="{{$product->title}}"></div>
                                 <div class="product_extra product_new"><a href="categories.html">New</a></div>
                                 <div class="product_content">
                                     <div class="product_title"><a href="product.html">{{$product->title}}</a></div>
