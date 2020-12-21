9b40a940127f60ba9de7fbc5b682b7b3c7816f42
Nickolay Grekalo <kolagrekalo@gmail.com>
2020-12-21 14:46:56 +0200

    created product page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,4 @@
-
+ 
 @extends("layouts.main")
 @section("title", "Home")
 @section("content")
@@ -144,7 +144,7 @@
                                 <div class="product_image"><img src="/images/{{$image}}" alt="{{$product->title}}"></div>
                                 <div class="product_extra product_new"><a href="categories.html">New</a></div>
                                 <div class="product_content">
-                                    <div class="product_title"><a href="product.html">{{$product->title}}</a></div>
+                                    <div class="product_title"><a href="{{route("showProduct", ["category", $product->id])}}">{{$product->title}}</a></div>
                                     <div class="product_price">{{$product->price}}$</div>
                                 </div>
                             </div>
