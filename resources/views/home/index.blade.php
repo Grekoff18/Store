c2d7b7acd0533412f02280404b5d37e2ef633f36
Nickolay Grekalo <kolagrekalo@gmail.com>
2020-12-21 17:06:00 +0200

    commit

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -144,8 +144,11 @@
                                 <div class="product_image"><img src="/images/{{$image}}" alt="{{$product->title}}"></div>
                                 <div class="product_extra product_new"><a href="categories.html">New</a></div>
                                 <div class="product_content">
-                                    <div class="product_title"><a href="{{route("showProduct", ["category", $product->id])}}">{{$product->title}}</a></div>
-                                    <div class="product_price">{{$product->price}}$</div>
+																		<div class="product_title"><a href="{{route("showProduct", ["category", $product->id])}}">{{$product->title}}</a></div>
+																		@if ($product->old_price !== null)
+																			<div style="text-decoration: line-through; color: red">${{$product->old_price}}</div>
+																		@endif
+                                    <div class="product_price">${{$product->price}}</div>
                                 </div>
                             </div>
                         @endforeach
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -41,12 +41,27 @@
 			<!-- Product Image -->
 			<div class="col-lg-6">
 				<div class="details_image">
-					<div class="details_image_large"><img src="/images/details_1.jpg" alt=""><div class="product_extra product_new"><a href="categories.html">New</a></div></div>
+
+
+					@php
+						$image = "";
+						if (count($item->images) > 0) {
+							$image = $item->images[0]["img"];
+						} else {
+							$image = "no_image.png";
+						}
+					@endphp
+					<div class="details_image_large"><img src="/images/{{$image}}" alt="{{$item->title}}"><div class="product_extra product_new"><a href="categories.html">New</a></div></div>
 					<div class="details_image_thumbnails d-flex flex-row align-items-start justify-content-between">
-						<div class="details_image_thumbnail active" data-image="/images/details_1.jpg"><img src="/images/details_1.jpg" alt=""></div>
-						<div class="details_image_thumbnail" data-image="/images/details_2.jpg"><img src="/images/details_2.jpg" alt=""></div>
-						<div class="details_image_thumbnail" data-image="/images/details_3.jpg"><img src="/images/details_3.jpg" alt=""></div>
-						<div class="details_image_thumbnail" data-image="/images/details_4.jpg"><img src="/images/details_4.jpg" alt=""></div>
+						@if ($image !== "no_image.png") 
+							@foreach ($item->images as $img)
+								@if ($loop->first)
+									<div class="details_image_thumbnail active" data-image="/images/{{$img['img']}}"><img src="/images/{{$img['img']}}" alt="{{$item->title}}"></div>
+								@else 
+									<div class="details_image_thumbnail" data-image="/images/{{$img['img']}}"><img src="/images/{{$img['img']}}" alt="{{$item->title}}"></div>
+								@endif
+							@endforeach
+						@endif
 					</div>
 				</div>
 			</div>
@@ -56,9 +71,9 @@
 				<div class="details_content">
 					<div class="details_name">{{$item->title}}</div>
 					@if ($item->new_price !== null)
-						<div class="details_discount">{{$item->new_price}}</div>
+						<div class="details_discount">${{$item->new_price}}</div>
 					@endif
-					<div class="details_price">{{$item->price}}</div>
+					<div class="details_price">${{$item->price}}</div>
 
 					<!-- In Stock -->
 					<div class="in_stock_container">
